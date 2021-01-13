413af0bc607748da0e21eeddf47ae673a05b0ad2
Grecia Delgado <greciadm2596@gmail.com>
2021-01-13 15:40:55 -0500

    add model Tweet , to be ahbel to create a tweet into apllication

---
+++ app/models/tweet.rb
@@ -0,0 +1,23 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id             :bigint           not null, primary key
+#  body           :text
+#  comments_count :integer          default(0)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :bigint           not null
+#
+# Indexes
+#
+#  index_tweets_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
