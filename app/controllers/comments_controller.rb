f25377e5009f3528021419ef506174c0d414b701
Grecia Delgado <greciadm2596@gmail.com>
2021-01-13 22:58:35 -0500

    Now we can delete a comment into a tweet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,40 @@
 class CommentsController < ApplicationController
-  
+  def edit
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = @tweet.comments.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = @tweet.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to tweets_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    comment = tweet.comments.find(params[:id])
+    comment.destroy
+    redirect_to tweets_path
+  end
+
+  def create
+    # @comment = Comments.new(tweet_params)
+    # @tweet.user = current_user
+    # if @tweet.save
+    #   redirect_to tweets_path
+    # else
+    #   @tweets = Tweet.all
+    #   render 'index'
+    # end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
