2222ecc6312ec3e8a3d7be67a01b6aacd882032e
Grecia Delgado <greciadm2596@gmail.com>
2021-01-13 19:44:43 -0500

    Now we can edit,destroy and edit a tweet in the view Index for controller Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,28 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to tweets_path
+  end
+
+  private 
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
 end
